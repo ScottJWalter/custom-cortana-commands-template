f976f1222fc1b4316b6514750685e51c749133a8
Scott Walter <sjwalter@gmail.com>
2020-07-07 08:46:39 -0500

    flashing out MQTT

--- CustomCortanaCommands/App.xaml.cs
+++ CustomCortanaCommands/App.xaml.cs
@@ -5,14 +5,12 @@ using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Navigation;
 
-using Windows.Media.SpeechRecognition;
-
 namespace CustomCortanaCommands
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
     /// </summary>
-    sealed partial class App : Application
+    internal sealed partial class App : Application
     {
         /// <summary>
         /// Initializes the singleton application object.  This is the first line of authored code
@@ -20,9 +18,6 @@ namespace CustomCortanaCommands
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -34,19 +29,15 @@ namespace CustomCortanaCommands
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-
 #if DEBUG
             if (System.Diagnostics.Debugger.IsAttached)
             {
                 this.DebugSettings.EnableFrameRateCounter = true;
             }
 #endif
-
-            Frame rootFrame = Window.Current.Content as Frame;
-
             // Do not repeat app initialization when the Window already has content,
             // just ensure that the window is active
-            if (rootFrame == null)
+            if (!(Window.Current.Content is Frame rootFrame))
             {
                 // Create a Frame to act as the navigation context and navigate to the first page
                 rootFrame = new Frame();
@@ -72,7 +63,7 @@ namespace CustomCortanaCommands
             // Ensure the current window is active
             Window.Current.Activate();
 
-            CortanaFunctions.RegisterVCD();      
+            CortanaFunctions.RegisterVCD();
         }
 
         protected override void OnActivated(IActivatedEventArgs args)
@@ -90,7 +81,7 @@ namespace CustomCortanaCommands
         /// </summary>
         /// <param name="sender">The Frame which failed navigation</param>
         /// <param name="e">Details about the navigation failure</param>
-        void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
+        public void OnNavigationFailed(object sender, NavigationFailedEventArgs e)
         {
             throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
         }
--- CustomCortanaCommands/CortanaFunctions.cs
+++ CustomCortanaCommands/CortanaFunctions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using Windows.UI.Popups;
 
 using Windows.Storage;
@@ -19,18 +20,21 @@ using Windows.Storage.Streams;
 using System.Diagnostics;
 using Windows.Web.Http;
 
+// MQTT
+using MQTTnet.Extensions.ManagedClient;
+using MQTTnet.Client.Options;
+using MQTTnet;
+using System.Threading.Tasks;
 
 namespace CustomCortanaCommands
 {
-
-    static class CortanaFunctions
+    public static class CortanaFunctions
     {
         /*
         This is the lookup of VCD CommandNames as defined in
-        CustomVoiceCommandDefinitios.xml to their corresponding actions
+        CustomVoiceCommandDefinitions.xml to their corresponding actions
         */
         private readonly static IReadOnlyDictionary<string, Delegate> vcdLookup = new Dictionary<string, Delegate>{
-
             /*
             {<command name from VCD>, (Action)(async () => {
                  <code that runs when that commmand is called>
@@ -38,28 +42,27 @@ namespace CustomCortanaCommands
             */
 
             {"OpenWebsite", (Action)(async () => {
-                 Uri website = new Uri(@"http://www.crclayton.com");
+                 Uri website = new Uri("http://www.crclayton.com");
                  await Launcher.LaunchUriAsync(website);
              })},
 
             {"OpenFile", (Action)(async () => {
-                StorageFile file = await Package.Current.InstalledLocation.GetFileAsync(@"Test.txt");
+                StorageFile file = await Package.Current.InstalledLocation.GetFileAsync("Test.txt");
                 await Launcher.LaunchFileAsync(file);
             })},
 
             {"CreateFile", (Action)(async () => {
                 StorageFolder storageFolder = ApplicationData.Current.LocalFolder;
                 StorageFile sampleFile = await storageFolder.CreateFileAsync(
-                    @"NewFile.txt", CreationCollisionOption.ReplaceExisting);
+                    "NewFile.txt", CreationCollisionOption.ReplaceExisting);
 
                 await storageFolder.GetFileAsync("NewFile.txt");
                 await FileIO.WriteTextAsync(sampleFile, "This file was created by Cortana at " + DateTime.Now);
-
             })},
 
             {"SendSerialData", (Action)(async () => {
-                string comPort = "COM3";
-                string serialMessage = "String sent to the COM port";
+                const string comPort = "COM3";
+                const string serialMessage = "String sent to the COM port";
 
                 string selector = SerialDevice.GetDeviceSelector(comPort);
                 DeviceInformationCollection devices = await DeviceInformation.FindAllAsync(selector);
@@ -86,6 +89,123 @@ namespace CustomCortanaCommands
             })},
 
             /*
+             * Command:  TODO
+             */
+            {"REST-POST", (Action)(async () => {
+                try
+                {
+                    Debug.WriteLine("Posting to REST API.");
+
+                    HttpClient httpClient = new HttpClient();
+                    // The base URL for the REST endpoint
+                    Uri uri = new Uri("");
+
+                    // Construct JSON to post
+                    // NOTE:  The following chunk is very API dependent, with each API possibly requiring
+                    // a different payload configuration
+                    const string APPLICATION_API_TOKEN = "";
+                    const string USER_KEY = "";
+                    const string MESSAGE = "You Have Been Pushed!"; // Or whatever you wish to send
+
+                    HttpStringContent content = new HttpStringContent(
+                        "{\"token\": \"" + APPLICATION_API_TOKEN + "\", \"user\": \"" + USER_KEY + "\", \"message\": \"" + MESSAGE + "\" }",
+                        UnicodeEncoding.Utf8,
+                        "application/json"
+                        );
+
+                    // Post the JSON and wait for a response.
+                    HttpResponseMessage httpResponseMessage = await httpClient.PostAsync( uri, content );
+
+                    // Make sure the post succeeded, and write out the response.
+                    httpResponseMessage.EnsureSuccessStatusCode();
+                    var httpResponseBody = await httpResponseMessage.Content.ReadAsStringAsync();
+                    Debug.WriteLine(httpResponseBody);
+                }
+                catch (Exception ex)
+                {
+                    // Write out any exceptions.
+                    Debug.WriteLine(ex);
+                }
+             })},
+
+            /*
+             * Command:  Hey Cortana, Listen Up! Initiate <script to run>
+             */
+            {"RunScript", (Action)(async () => {
+                string script_name = CortanaFunctions.GetSlotData("free_form_text");
+
+                try
+                {
+                    Debug.WriteLine("Run script here.");
+
+                    await CortanaFunctions
+                        .ExecuteScript(script_name)
+                        .ConfigureAwait(false)
+                        ;
+
+                    Debug.WriteLine("Bat file executed !!");
+                }
+                catch (Exception ex)
+                {
+                    // Write out any exceptions.
+                    Debug.WriteLine(ex);
+                }
+             })},
+
+            /*
+             * Command:  Hey Cortana, Listen up!  Tell <device> <command>
+             */
+            {"SendMQTT", (Action)(async () => {
+                string device = CortanaFunctions.GetSlotData("MQTT_device");
+                string message = CortanaFunctions.GetSlotData("free_form_text");
+
+                try
+                {
+                    Debug.WriteLine("Send MQTT here.");
+
+                    string clientId = Guid.NewGuid().ToString();
+                    const string mqttURI = "";
+                    const string mqttUser = "";
+                    const string mqttPassword = "";
+                    const int mqttPort = 0;
+                    const bool mqttSecure = false;
+
+                    var messageBuilder = new MqttClientOptionsBuilder()
+                        .WithClientId(clientId)
+                        .WithCredentials(mqttUser, mqttPassword)
+                        .WithTcpServer(mqttURI, mqttPort)
+                        .WithCleanSession()
+                        ;
+
+                    var options = mqttSecure
+                        ? messageBuilder
+                            .WithTls()
+                            .Build()
+                        : messageBuilder
+                            .Build()
+                        ;
+
+                    var managedOptions = new ManagedMqttClientOptionsBuilder()
+                        .WithAutoReconnectDelay(TimeSpan.FromSeconds(5))
+                        .WithClientOptions(options)
+                        .Build()
+                        ;
+
+                    IManagedMqttClient client = new MqttFactory().CreateManagedMqttClient();
+
+                    await client
+                        .StartAsync(managedOptions)
+                        .ConfigureAwait(false)
+                        ;
+                }
+                catch (Exception ex)
+                {
+                    // Write out any exceptions.
+                    Debug.WriteLine(ex);
+                }
+             })},
+
+            /*
              * Command:  Hey Cortana, Listen Up! Send Push.
              */
             {"PushIt", (Action)(async () => {
@@ -101,9 +221,9 @@ namespace CustomCortanaCommands
                      *        security purposes.  THIS IS JUST A SIMPLE EXMPLE.  CAVEAT EMPTOR.
                      */
                     // Construct JSON to post
-                    string APPLICATION_API_TOKEN = ""; // Generated at https://pushover.net/apps/build
-                    string USER_KEY = ""; // Found on your dashboard at https://pushover.net
-                    string MESSAGE = "You Have Been Pushed!"; // Or whatever you wish to send
+                    const string APPLICATION_API_TOKEN = ""; // Generated at https://pushover.net/apps/build
+                    const string USER_KEY = ""; // Found on your dashboard at https://pushover.net
+                    const string MESSAGE = "You Have Been Pushed!"; // Or whatever you wish to send
 
                     HttpStringContent content = new HttpStringContent(
                         "{\"token\": \"" + APPLICATION_API_TOKEN + "\", \"user\": \"" + USER_KEY + "\", \"message\": \"" + MESSAGE + "\" }",
@@ -127,16 +247,70 @@ namespace CustomCortanaCommands
              })},
         };
 
+        /**
+         * A local copy of the SpeechRecognitionResult returned from Cortana, necessary to grab the contents
+         * of data slots in the intent.
+         */
+        private static SpeechRecognitionResult _srr = null;
+
         /*
         Register Custom Cortana Commands from VCD file
         */
         public static async void RegisterVCD()
         {
-            StorageFile vcd = await Package.Current.InstalledLocation.GetFileAsync(
-                @"CustomVoiceCommandDefinitions.xml");
+            try
+            {
+                StorageFile vcd = await Package.Current.InstalledLocation.GetFileAsync("CustomVoiceCommandDefinitions.xml");
+                await VoiceCommandDefinitionManager.InstallCommandDefinitionsFromStorageFileAsync(vcd);
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Debug.WriteLine("There was an error registering the Voice Command Definitions", ex);
+            }
+        }
 
-            await VoiceCommandDefinitionManager
-                .InstallCommandDefinitionsFromStorageFileAsync(vcd);
+        public static string GetSlotData(string interpretationKey)
+        {
+            return CortanaFunctions._srr.SemanticInterpretation.Properties[interpretationKey].FirstOrDefault();
+        }
+
+        public static async Task<int> ExecuteScript(string fileName, string args = null)
+        {
+            using (var process = new Process
+            {
+                StartInfo =
+                {
+                    FileName = fileName, Arguments = args,
+                    UseShellExecute = false, CreateNoWindow = true,
+                    RedirectStandardOutput = true, RedirectStandardError = true
+                },
+                EnableRaisingEvents = true
+            })
+            {
+                return await CortanaFunctions.RunProcessAsync(process).ConfigureAwait(false);
+            }
+        }
+
+        private static Task<int> RunProcessAsync(Process process)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            process.Exited += (s, ea) => tcs.SetResult(process.ExitCode);
+            process.OutputDataReceived += (s, ea) => Console.WriteLine(ea.Data);
+            process.ErrorDataReceived += (s, ea) => Console.WriteLine("ERR: " + ea.Data);
+
+            bool started = process.Start();
+            if (!started)
+            {
+                //you may allow for the process to be re-used (started = false)
+                //but I'm not sure about the guarantees of the Exited event in such a case
+                throw new InvalidOperationException("Could not start process: " + process);
+            }
+
+            process.BeginOutputReadLine();
+            process.BeginErrorReadLine();
+
+            return tcs.Task;
         }
 
         /*
@@ -144,8 +318,8 @@ namespace CustomCortanaCommands
         */
         public static void RunCommand(VoiceCommandActivatedEventArgs cmd)
         {
-            SpeechRecognitionResult result = cmd.Result;
-            string commandName = result.RulePath[0];
+            CortanaFunctions._srr = cmd.Result;
+            string commandName = CortanaFunctions._srr.RulePath[0];
             vcdLookup[commandName].DynamicInvoke();
         }
     }
